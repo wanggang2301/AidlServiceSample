80ee030e8110b81ebfe037ba0df34163f2d40a4a
wangg <wangg@13322.com>
2017-03-06 10:40:01 +0800

    aidl service sample

---
+++ app/src/main/aidl/com/wg/aidlsample/ICat.aidl
@@ -0,0 +1,9 @@
+// ICat.aidl
+package com.wg.aidlsample;
+
+// Declare any non-default types here with import statements
+
+interface ICat {
+    String getColor();
+      	double getWeight();
+}
---
+++ app/src/main/java/com/wg/aidlservicesample/AidlService.java
@@ -0,0 +1,81 @@
+package com.wg.aidlservicesample;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
+
+import com.wg.aidlsample.ICat;
+
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * @author: Wangg
+ * @Name：AidlService
+ * @Description:  Aidl的Service服务 类似服务器段
+ * @Created on:2017/3/3  16:44.
+ */
+
+
+//Aidl的Service服务
+
+public class AidlService extends Service {
+
+    private CatBinder catBinder;
+
+    Timer timer = new Timer();
+    String[] colors = new String[]{
+            "红色", "黄色", "黑色", "北京", "上海", "深圳", "广州"
+    };
+
+    double[] weights = new double[]{
+            2.3, 3.1, 1.58, 100, 200, 300, 400
+    };
+
+    private String color;
+    private double weight;
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return catBinder;
+    }
+
+
+    public class CatBinder extends ICat.Stub {
+
+        @Override
+        public String getColor() throws RemoteException {
+            return color;
+        }
+
+        @Override
+        public double getWeight() throws RemoteException {
+            return weight;
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        catBinder = new CatBinder();
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                int rand = new Random().nextInt(7);
+                color = colors[rand];
+                weight = weights[rand];
+                Log.d("ddf", "_______" + rand);
+            }
+        }, 0, 800);
+    }
+
+    @Override
+    public void onDestroy() {
+        timer.cancel();
+    }
+}
